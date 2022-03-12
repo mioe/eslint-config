899f6e9bda5261fc20fe6451bec62182494cc457
mioe <mioe.boss@gmail.com>
2022-03-12 11:34:49 +0300

    try init first package

---
+++ packages/base/index.js
@@ -0,0 +1,37 @@
+
+module.exports = {
+	env: {
+		es6: true,
+		browser: true,
+		node: true,
+	},
+	extends: [
+		'eslint:recommended',
+	],
+	parserOptions: {
+		ecmaVersion: 2022,
+	},
+	ignorePatterns: [
+		'*.min.*',
+		'CHANGELOG.md',
+		'dist',
+		'LICENSE*',
+		'output',
+		'coverage',
+		'public',
+		'temp',
+		'packages-lock.json',
+		'pnpm-lock.yaml',
+		'yarn.lock',
+		'__snapshots__',
+		'!.github',
+		'!.vitepress',
+		'!.vscode',
+	],
+	rules: {
+		'comma-dangle': ['error', 'always-multiline'],
+		'quotes': ['error', 'single'],
+		'semi': ['error', 'never'],
+		'space-before-function-paren': ['error', 'never'],
+	},
+}
