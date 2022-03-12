943203c2cd426bef77ad6947cdf5df8cbf0fca32
mioe <mioe.boss@gmail.com>
2022-03-12 22:04:02 +0300

    add vue and ts

---
+++ packages/ts/index.js
@@ -0,0 +1,10 @@
+module.exports = {
+	extends: [
+		'@mioe/eslint-config-base',
+		'plugin:@typescript-eslint/recommended',
+	],
+	rules: {
+		"@typescript-eslint/ban-ts-comment": "off",
+		"@typescript-eslint/no-explicit-any": "off",
+	}
+}
---
+++ packages/vue/index.js
@@ -0,0 +1,32 @@
+module.exports = {
+	overrides: [
+		{
+			files: ['*.vue'],
+			parser: 'vue-eslint-parser',
+			parserOptions: {
+				parser: '@typescript-eslint/parser',
+			},
+			rules: {
+				'no-unused-vars': 'off',
+				'no-undef': 'off',
+				'@typescript-eslint/no-unused-vars': 'off',
+			},
+		},
+	],
+	extends: [
+		'plugin:vue/vue3-recommended',
+		'@mioe/eslint-config-ts',
+	],
+	rules: {
+		'vue/multi-word-component-names': 'off',
+		'vue/require-default-prop': 'off',
+		'vue/require-prop-types': 'off',
+		"vue/html-indent": ['error', 'tab', {
+			'attribute': 1,
+			'baseIndent': 1,
+			'closeBracket': 0,
+			'alignAttributesVertically': true,
+			'ignores': []
+		}]
+	},
+}
